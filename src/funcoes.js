9e154326eb01405952817eb3d1358037ff9ca69d
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-04-01 21:01:59 -0300

    Uso de casting em typescript

--- src/funcoes.js
+++ src/funcoes.js
@@ -1,28 +1,22 @@
-
+"use strict";
 // Este código TypeScript apresenta exemplos de funções e retorno condicional:
-
 // calculaArea: Uma função que calcula a área de um retângulo com base e altura fornecidas.
 // calculaArea2: Uma expressão de função que também calcula a área de um retângulo.
 // somar: Uma função que recebe um número variável de argumentos e os imprime no console.
 // teste: Uma função que retorna uma string se a condição for verdadeira e um número se for falsa.
-
-function calculaArea(base: number, altura: number): number{
-    return base * altura
+function calculaArea(base, altura) {
+    return base * altura;
 }
-
-const calculaArea2 = (base: number, altura: number ): number => base * altura;
-
-function somar (... numeros: number[]): void {
+const calculaArea2 = (base, altura) => base * altura;
+function somar(...numeros) {
     // numeros.reduce()
     console.log(numeros);
-    
 }
-
-function teste(): string | number {
-    if(10 > 5){
-        return "10 é maior que 5"
-    } else {
-        return 5
+function teste() {
+    if (10 > 5) {
+        return "10 é maior que 5";
+    }
+    else {
+        return 5;
     }
 }
-
---
+++ src/tipos_basicos.js
@@ -0,0 +1,25 @@
+"use strict";
+// 
+// Este código TypeScript demonstra a utilização de diferentes tipos de variáveis e estruturas de dados:
+// boolean: Representa valores verdadeiro/falso (true/false).
+// number: Representa valores numéricos, tanto inteiros quanto de ponto flutuante.
+// string: Representa valores de texto.
+// string[] ou Array<string>: Representa um array de strings.
+// ReadonlyArray<number>: Representa um array de números somente para leitura.
+// Tupla: Uma estrutura de dados que pode conter diferentes tipos de valores em posições específicas.
+// any: Representa um tipo que pode ser qualquer coisa.
+let estaChovendo = false;
+estaChovendo = true;
+let idade = 35;
+let altura = 1.75;
+const nacionalidade = "brasileira";
+const colegas = ["sanderson", "melanie", "nacionalidade"];
+const tecnologias = ["html", "css", "js"];
+const notas = [7, 9, 5, 8];
+const lista = ["sanderson", true, 35];
+let idadeDaAna = "25";
+idadeDaAna = 25;
+let dadosDaApi;
+dadosDaApi = "nome";
+dadosDaApi = 10;
+dadosDaApi = true;
